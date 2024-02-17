557bb0970ec4f2895951fd3e42d6149fcf5a52eb
Dr. Hans-Peter Stoerr <hans-peter+git-noreply@stoerr.net>
2024-02-17 12:31:44 +0100

    ChatGPT: Read the README.md and the Implementation.md, list all files in the project, determine what needs to be done to generate a rough frame for the project. Do not generate any code except the main class for the command line, it should for now just print the usage. Do generate the maven modules and run the build action to make sure it works.
    Before doing anything list the files to make sure it hasn't been done already, and continue from there writing the necessary files yourself and execute the build action afterwards.

---
+++ aigenpipeline-commandline/src/main/java/net/stoerr/ai/aigenpipeline/commandline/AIGenPipeline.java
@@ -0,0 +1,9 @@
+package net.stoerr.ai.aigenpipeline.commandline;
+
+public class AIGenPipeline {
+
+    public static void main(String[] args) {
+        System.out.println("Usage: aigenpipeline [options] [<input_files>...]");
+    }
+
+}
