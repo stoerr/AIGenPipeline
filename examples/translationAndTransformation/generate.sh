9627c3ff22c47993a7a8aa6f9287d1570d7d02f9
Dr. Hans-Peter Stoerr <hans-peter+git-noreply@stoerr.net>
2024-02-25 23:00:19 +0100

    improve translation and rewriting example

--- examples/translationAndTransformation/generate.sh
+++ examples/translationAndTransformation/generate.sh
@@ -2,5 +2,7 @@
 CMD="../../bin/aigenpipeline"
 # CMD="$CMD -v"
 set -v -e
-$CMD -t 2048 0predefinedprompts.json -p 0translate.prompt -o 1germanprompts.json
-$CMD -t 4096 1germanprompts.json -p 1transformToPage.prompt -o 2page.xml
+for lang in de es fr; do
+  $CMD -t 2048 0predefinedprompts.json -p 0translate.prompt -o 1prompts-${lang}.json -k "lang=$lang"
+  $CMD -t 4096 1prompts-${lang}.json -p 1transformToPage.prompt -o 2page-${lang}.xml
+done
